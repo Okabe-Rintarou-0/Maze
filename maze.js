0b99fb26fc86c0578429181bb0ecd8128a309758
Okabe-Rintarou-0 <58595459+Okabe-Rintarou-0@users.noreply.github.com>
2020-11-22 16:50:31 +0800

    update some func

--- maze.js
+++ maze.js
@@ -18,9 +18,15 @@ var setPoint_Counter = 0;
 var presentCanvasWidth;
 var presentCanvasHeight;
 var presentReqId = [];
-var direction = [[0, -gridSize], [0, gridSize], [-gridSize, 0], [gridSize, 0]];
+var direction = [[0, -gridSize], [0, gridSize], [-gridSize, 0], [gridSize, 0],
+    [-gridSize, -gridSize], [gridSize, -gridSize], [-gridSize, gridSize], [gridSize, gridSize]];
 var lastState = true;
 var Animated = true;
+var diagonalLosses = 14;
+var verticalLosses = 10;
+var horizontalLosses = 10;
+var G = [];
+var select;
 
 function toggle() {
     let animationRadio = document.getElementsByClassName('AnimationRadio');
@@ -28,6 +34,29 @@ function toggle() {
         animationRadio[0].checked = !animationRadio[0].checked;
     lastState = animationRadio[0].checked;
     Animated = lastState;
+}
+
+function ResetMaze() {
+    // alert(1);
+    let totalStep = document.getElementsByClassName('totalStep');
+    totalStep[0].innerHTML = 'totalStep:null';
+    totalStep[1].innerHTML = 'totalStep:null';
+    for (let index = 0; index < 2; index++)
+        cancelAnimationFrame(presentReqId[index]);
+    for (let index = 0; index < 2; index++)
+        while (visitedPoints[index].length)
+            visitedPoints[index].pop();
+    copyMaze(2, 0);
+    copyMaze(2, 1);
+    setDomain(0, startX, startY, 1, gridSize);
+    setDomain(1, startX, startY, 1, gridSize);
+    setDomain(0, targetX, targetY, 1, gridSize);
+    setDomain(1, targetX, targetY, 1, gridSize);
+    setPoint_Counter = 0;
+    drawMaze(leftCanvas, 0);
+    drawMaze(rightCanvas, 1);
+    startPoint.innerText = 'targetPoint: (null,null)';
+    targetPoint.innerText = 'targetPoint: (null,null)';
 }
 
 function onClick() {
@@ -51,8 +80,9 @@ function onClick() {
     for (let index = 0; index < 2; index++)
         while (visitedPoints[index].length)
             visitedPoints[index].pop();
+    let delta = Math.floor(gridSize / 2);
 
-    formMaze(0, 1, 1);
+    formMaze(0, delta, delta);
     copyMaze(0, 1);
     drawMaze(leftCanvas, 0);
     drawMaze(rightCanvas, 1);
@@ -76,6 +106,7 @@ function clearCordinate() {
     presentPoint.innerText = 'presentPoint: (null,null)';
 }
 
+//TODO:update this fucking function;
 function normalize(x_or_y) {
     if (x_or_y % 3 === 0) x_or_y += 1;
     else if (x_or_y >= 2 && (x_or_y - 2) % 3 === 0)
@@ -89,8 +120,8 @@ function drawStartAndTargetPoint() {
     mouseY = normalize(mouseY);
     if (!isBlock(0, mouseX, mouseY)) {
         setPoint_Counter++;
-        setDomain(0, mouseX, mouseY, setPoint_Counter + 1);
-        setDomain(1, mouseX, mouseY, setPoint_Counter + 1);
+        setDomain(0, mouseX, mouseY, setPoint_Counter + 1, gridSize);
+        setDomain(1, mouseX, mouseY, setPoint_Counter + 1, gridSize);
     } else return;
     if (setPoint_Counter === 1) {
         startX = mouseX;
@@ -100,19 +131,36 @@ function drawStartAndTargetPoint() {
         targetX = mouseX;
         targetY = mouseY;
         targetPoint.innerHTML = 'targetPoint: (' + targetX.toString() + ',' + targetY.toString() + ')';
+        copyMaze(0, 2);
     }
 
+    initG();
 
     drawMaze(leftCanvas, 0);
     drawMaze(rightCanvas, 1);
     if (setPoint_Counter === 2) {
-        searchForPath_DFS(0, startX, startY, Animated);
-        searchForPath_DFS_NEW(1, startX, startY, Animated);
+        for (let index = 0; index < 2; index++) {
+            // alert(select[index].selectedIndex)
+            switch (select[index].selectedIndex) {
+                case 0:
+                    searchForPath_AStar(index, startX, startY, Animated);
+                    break;
+                case 1:
+                    searchForPath_DFS(index, startX, startY, Animated);
+                    break;
+                case 2:
+                    searchForPath_DFS_NEW(index, startX, startY, Animated);
+                    break;
+            }
+        }
+
         let totalStep = document.getElementsByClassName('totalStep');
         for (let index = 0; index < 2; index++) {
             totalStep[index].innerHTML = 'totalStep:' + (visitedPoints[index].length / 3).toString();
-            if (Animated) drawPath(index, 0);
-            else {
+            if (Animated) {
+                copyMaze(2, index);
+                drawPath(index, 0);
+            } else {
                 drawMaze(leftCanvas, 0);
                 drawMaze(rightCanvas, 1);
             }
@@ -120,14 +168,91 @@ function drawStartAndTargetPoint() {
     }
 }
 
-function searchForPath_DFS_NEW(index, presentX, presentY, Animated = true) {
+
+function chebyshevDistance(x1, y1, x2, y2) {
+    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+}
+
+function canApproach(index, tgtx, tgty, dir) {
+    switch (dir) {
+        case 4:
+            return !isBlock(index, tgtx + gridSize, tgty) && !isBlock(index, tgtx, tgty - gridSize);
+        case 5:
+            return !isBlock(index, tgtx, tgty - gridSize) && !isBlock(index, tgtx - gridSize, tgty);
+        case 6:
+            return !isBlock(index, tgtx + gridSize, tgty) && !isBlock(index, tgtx, tgty - gridSize);
+        case 7:
+            return !isBlock(index, tgtx, tgty - gridSize) && !isBlock(index, tgtx - gridSize, tgty);
+        default:
+            return true;
+    }
+}
+
+//当前存在的问题 没有处理好回溯 回溯之后应走其他次优的解
+function searchForPath_AStar(index, presentX, presentY, Animated = true) {
     if (presentX === targetX && presentY === targetY) {
+        setDomain(index, targetX, targetY, 5, gridSize);
         visitedPoints[index].push(presentX);
         visitedPoints[index].push(presentY);
         visitedPoints[index].push(5);
         return true;
     }
-    setDomain(index, presentX, presentY, 4);
+    setDomain(index, presentX, presentY, 4, gridSize);
+    visitedPoints[index].push(presentX);
+    visitedPoints[index].push(presentY);
+    visitedPoints[index].push(4);
+    while (1) {
+        let bestNextX = -1;
+        let bestNextY = -1;
+        let MIN = Number.MAX_VALUE;
+        for (let i = 0; i < 8; i++) {
+            let nextX = presentX + direction[i][0];
+            let nextY = presentY + direction[i][1];
+            // alert(nextX.toString() + ',' + nextY.toString());
+            if (inRange(nextX, nextY) && !isBlock(index, nextX, nextY) && !hasVisited(index, nextX, nextY) && canApproach(index, nextX, nextY, i)) {
+                let g = G[presentX][presentY] = i >= 4 ? diagonalLosses : horizontalLosses;
+                let h = chebyshevDistance(nextX, nextY, targetX, targetY);
+                let losses = g + h;
+                // alert(i);
+                if (losses <= MIN) {
+                    bestNextX = nextX;
+                    bestNextY = nextY;
+                    MIN = losses;
+                }
+            }
+        }
+        if (bestNextX === -1) break;
+        // alert(bestNextX.toString() + ' , ' + bestNextY.toString());
+        G[bestNextX][bestNextY] += MIN;
+        // drawMaze(rightCanvas, 1);
+        // alert('f');
+        if (searchForPath_AStar(index, bestNextX, bestNextY, Animated)) {
+            if (Animated) setDomain(index, bestNextX, bestNextX, 1, gridSize);
+            return true;
+        }
+        G[bestNextX][bestNextY] -= MIN;
+        // drawMaze(rightCanvas, 1);
+    }
+    // setDomain(index, presentX, presentY, 1);
+    visitedPoints[index].push(presentX);
+    visitedPoints[index].push(presentY);
+    visitedPoints[index].push(1);
+    return false;
+    // alert(presentX.toString() + ',' + presentY.toString() + '  ' + bestNextX.toString() + ',' + bestNextY.toString());
+    // alert('finished');
+
+}
+
+
+function searchForPath_DFS_NEW(index, presentX, presentY, Animated = true) {
+    if (presentX === targetX && presentY === targetY) {
+        setDomain(index, targetX, targetY, 5, gridSize);
+        visitedPoints[index].push(presentX);
+        visitedPoints[index].push(presentY);
+        visitedPoints[index].push(5);
+        return true;
+    }
+    setDomain(index, presentX, presentY, 4, gridSize);
     visitedPoints[index].push(presentX);
     visitedPoints[index].push(presentY);
     visitedPoints[index].push(4);
@@ -143,12 +268,12 @@ function searchForPath_DFS_NEW(index, presentX, presentY, Animated = true) {
             // alert(presentX.toString() + ',' + presentY.toString());
             // drawMaze(leftCanvas, 0);
             if (searchForPath_DFS_NEW(index, nextX, nextY, Animated)) {
-                if (Animated) setDomain(index, presentX, presentY, 1);
+                if (Animated) setDomain(index, presentX, presentY, 1, gridSize);
                 return true;
             }
         }
     }
-    setDomain(index, presentX, presentY, 1);
+    setDomain(index, presentX, presentY, 1, gridSize);
     visitedPoints[index].push(presentX);
     visitedPoints[index].push(presentY);
     visitedPoints[index].push(1);
@@ -158,12 +283,13 @@ function searchForPath_DFS(index, presentX, presentY, Animated = true) {
 
     // console.log(visitedPoints.length);
     if (presentX === targetX && presentY === targetY) {
+        setDomain(index, targetX, targetY, 5, gridSize);
         visitedPoints[index].push(presentX);
         visitedPoints[index].push(presentY);
         visitedPoints[index].push(5);
         return true;
     }
-    setDomain(index, presentX, presentY, 4);
+    setDomain(index, presentX, presentY, 4, gridSize);
     visitedPoints[index].push(presentX);
     visitedPoints[index].push(presentY);
     visitedPoints[index].push(4);
@@ -174,13 +300,13 @@ function searchForPath_DFS(index, presentX, presentY, Animated = true) {
             // alert(presentX.toString() + ',' + presentY.toString());
             // drawMaze(leftCanvas, 0);
             if (searchForPath_DFS(index, nextX, nextY, Animated)) {
-                if (Animated) setDomain(index, presentX, presentY, 1);
+                if (Animated) setDomain(index, presentX, presentY, 1, gridSize);
                 return true;
             }
         }
     }
     if (Animated)
-        setDomain(index, presentX, presentY, 1);
+        setDomain(index, presentX, presentY, 1, gridSize);
     visitedPoints[index].push(presentX);
     visitedPoints[index].push(presentY);
     visitedPoints[index].push(1);
@@ -199,7 +325,7 @@ function drawPath(canvasIndex, drawIndex, method = 1) {
     let y = visitedPoints[canvasIndex][drawIndex * 3 + 1];
     let colorValue = visitedPoints[canvasIndex][drawIndex * 3 + 2];
     // alert(x.toString() + ',' + y.toString() + ',' + colorValue.toString());
-    setDomain(canvasIndex, x, y, colorValue);
+    setDomain(canvasIndex, x, y, colorValue, gridSize);
     if (canvasIndex === 0)
         drawMaze(leftCanvas, canvasIndex);
     else
@@ -216,13 +342,13 @@ function initCanvas(canvas, canvasColor = 'white') {
 }
 
 function initMaze() {
-    Mazes = new Array(2);
-    for (let i = 0; i < 2; i++) {
+    Mazes = new Array(3);
+    for (let i = 0; i < 3; i++) {
         Mazes[i] = new Array(presentCanvasWidth);
         for (let j = 0; j < presentCanvasWidth; j++)
             Mazes[i][j] = new Array(presentCanvasHeight);
     }
-    for (let i = 0; i < 2; i++)
+    for (let i = 0; i < 3; i++)
         for (let j = 0; j < presentCanvasWidth; j++)
             for (let k = 0; k < presentCanvasHeight; k++)
                 Mazes[i][j][k] = 0;
@@ -250,7 +376,7 @@ function setCanvasSize(canvas, width, height) {
 function drawRect(canvas, x, y, color, size = 3) {
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = color;
-    ctx.fillRect(x - 1, y - 1, size, size);
+    ctx.fillRect(x, y, size, size);
     // alert(isPath(leftCanvas, x, y));
 }
 
@@ -271,15 +397,15 @@ function getPixelData(canvas, x, y) {
 function drawLines(index, x, y, dir) {
     if (dir === 0)//横向
         for (let i = x; i < presentCanvasWidth; i += gridSize)
-            setDomain(index, i, y, 0);
+            setDomain(index, i, y, 0, gridSize);
     else
         for (let j = y; j < presentCanvasHeight; j += gridSize)
-            setDomain(index, x, j, 0);
+            setDomain(index, x, j, 0, gridSize);
 }
 
 
 function inRange(x, y) {
-    return !(x >= presentCanvasWidth || y >= presentCanvasHeight || x < 1 || y < 1);
+    return !(x >= presentCanvasWidth || y >= presentCanvasHeight || x < Math.floor(gridSize / 2) || y < Math.floor(gridSize / 2));
 }
 
 function drawMaze(canvas, index) {
@@ -299,9 +425,11 @@ function drawMaze(canvas, index) {
         }
 }
 
-function setDomain(index, x, y, value) {
-    for (let i = -1; i <= 1; i++)
-        for (let j = -1; j <= 1; j++) {
+function setDomain(index, x, y, value, gridSize) {
+    let delta = Math.floor(gridSize / 2);
+    // alert(delta);
+    for (let i = -delta; i <= delta; i++)
+        for (let j = -delta; j <= delta; j++) {
             let X = x + i;
             let Y = y + j;
             if (X <= presentCanvasWidth && Y <= presentCanvasHeight && X >= 0 && Y >= 0)
@@ -382,7 +510,7 @@ function formMaze(index, x, y) {
     // if (times % 5000 === 0) {
     //     drawMaze(leftCanvas, 0)
     // }
-    setDomain(index, x, y, 1);
+    setDomain(index, x, y, 1, gridSize);
     let nextX;
     let nextY;
     // alert(x.toString() + ' ' + y.toString());
@@ -408,8 +536,13 @@ function closeModal() {
     document.getElementById('modal-id').classList.remove('active');
 }
 
+function initG() {
+    for (let i = 0; i < presentCanvasWidth; i++)
+        for (let j = 0; j < presentCanvasHeight; j++)
+            G[i][j] = 0;
+}
+
 function init() {
-    gridSize = 3;
     leftCanvas = document.getElementById("leftCanvas");
     rightCanvas = document.getElementById("rightCanvas");
     inputWidth = document.getElementById("inputWidth");
@@ -437,15 +570,35 @@ function init() {
     visitedPoints[0] = [];
     visitedPoints[1] = [];
 
+    for (let i = 0; i < presentCanvasWidth; i++) {
+        G[i] = new Array(presentCanvasHeight);
+    }
+    initG();
+
     let animationRadio = document.getElementsByClassName('AnimationRadio');
     animationRadio[0].checked = true;
 
     initCanvas(leftCanvas);
     initCanvas(rightCanvas);
 
+    select = document.getElementsByClassName("form-select");
+
     initMaze();
-    formMaze(0, 1, 1);
+
+    let delta = Math.floor(gridSize / 2);
+
+    formMaze(0, delta, delta);
     copyMaze(0, 1);
+    copyMaze(0, 2);
     drawMaze(leftCanvas, 0);
     drawMaze(rightCanvas, 1);
 }
+
+$('body').ready(function () {
+    $('#MazeIcon').fadeIn(1000, function () {
+        let b = $('b.animated');
+        b.animate({left: '-=220px', opacity: '1'});
+        b.animate({left: '+=40px'});
+        b.animate({left: '-=20px'});
+    });
+});
